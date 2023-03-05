9904eecc9cf3f4ca5e577bb47d351b2ba8fb68d7
artemijsS <artjom1408@gmail.com>
2023-03-05 22:58:40 +0200

    seo

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useRouter } from "next/router";
 import Head from "next/head";
 import Header from "./Header";
 
@@ -6,14 +7,33 @@ interface MainContainer {
     children?: React.ReactNode,
     title?: string,
     description?: string,
+    keywords?: string
 }
 
-const MainContainer = ({ children, title, description }: MainContainer) => {
+const MainContainer = ({ children, title, description, keywords }: MainContainer) => {
+
+    const router = useRouter()
+
     return (
         <>
             <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta name="theme-color" content="#FFF" />
+                <meta name="author" content="energyplus" />
                 {title && <title>{title}</title>}
                 {description && <meta name="description" content={description} />}
+                {keywords && <meta name="keywords" content={keywords} />}
+
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://www.energyplus.lv" />
+                <meta property="og:img" content="https://www.energyplus.lv/icon_192.png" />
+                {title && <meta property="og:title" content={title} />}
+                {description && <meta property="og:title" content={description} />}
+
+                <link rel="alternate" hrefLang="en" href={"https://www.energyplus.lv" + router.pathname}/>
+                <link rel="alternate" hrefLang="lv" href={"https://www.energyplus.lv/lv" + router.pathname}/>
+                <link rel="alternate" hrefLang="ru" href={"https://www.energyplus.lv/ru" + router.pathname}/>
+
             </Head>
             <Header />
             {children}
